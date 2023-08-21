c15f17cf0e099f9c4741bf1f2b9edfa11146ae4b
Alexey Rozhnov <justgast@gmail.com>
2023-08-21 11:45:56 +0300

    Getting events from 00:00 of the first day;

--- internal/calendar_utils/calendar.go
+++ internal/calendar_utils/calendar.go
@@ -3,6 +3,11 @@ package calendar_utils
 import "time"
 
 func GetStartDay() time.Time {
-	t := time.Now().AddDate(0, 0, -time.Now().Day()+1)
+	now := time.Now()
+	// Get first day of month
+	t := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location()).
+		AddDate(0, 0, -time.Now().Day()+1)
+
+	// Get first monday before first day of month
 	return t.AddDate(0, 0, -int(t.Weekday())+1)
 }
